aaa57a65933f604afaa9b9292aa185ad125ecc1b
Alice <achen@captechventures.com>
2021-04-18 20:17:36 -0400

    finish tour of heroes

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -10,7 +10,8 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero;
+  // **QUESTION: what is the best way to handle this (throws error when i don't initialize, when i make nullable then there is a problem with updateHero(this.hero) because hero can be undefined)? stackoverflow says to disable requiring initialization but i kind of like having stricter rules
+  hero: Hero = { id: -1, name: 'placeholder' };
 
   constructor(
     private route: ActivatedRoute,
@@ -29,5 +30,9 @@ export class HeroDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 }
---
+++ angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of, Subject } from 'rxjs';
+
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css'],
+})
+export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]> = of([]);
+  // RxJS Subject - is both a source of observable values & an Observable itself. You can subscribe to a Subject as you would any Observable
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) {}
+
+  // passing a new search term directly to teh searchHeroes() after every user keystroke would create an excessive amount of HTTP requests, taxing server resources & burning through data plans
+  // this method pipes the searchTerms observable through a sequence of RxJS operators that reduce the number of calls to the searchHeroes(), ultimately returning an observable of timely hero search results (each a Hero[])
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
+  }
+
+  search(term: string): void {
+    // push values into searchTerms
+    // every time the user types in the textbox, the binding calls search() with the textbox value, a "search term"
+    // searchTerms becomes an Observable emitting a steady stream of search terms
+    this.searchTerms.next(term);
+    console.log(term);
+  }
+}
