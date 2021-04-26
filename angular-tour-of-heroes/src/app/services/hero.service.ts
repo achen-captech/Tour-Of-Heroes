b2851e42282180fff712ea642f7498b462587d9a
Alice <achen@captechventures.com>
2021-04-26 14:07:30 -0400

    organize directory structure

--- angular-tour-of-heroes/src/app/services/hero.service.ts
+++ angular-tour-of-heroes/src/app/services/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
+import { Hero } from '../models/hero';
 import { Observable, of } from 'rxjs';
-import { MessageService } from './message.service';
+import { MessageService } from '../services/message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
